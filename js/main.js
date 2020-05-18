f32ea0fd9174dd16242de351bb8b7a7252f3b762
brandongiampa <30915668+brandongiampa@users.noreply.github.com>
2020-05-17 20:45:32 -0700

    set up open and close mobile menu

--- js/main.js
+++ js/main.js
@@ -6,10 +6,53 @@ if (
 } else {
   document.addEventListener("DOMContentLoaded", onLoad)
 }
+var mobileMenuIsOpen=false
 
 function onLoad(){
   fontify("#nav-header-pc ul li")
   fontify("#nav-header-mobile ul li")
+  document.querySelector("#menu-expand i").addEventListener('click', openOrCloseMobileNavMenu)
+}
+function unblacken(){
+  let headerAndFooterOpaques = document.getElementsByClassName("blackout")
+  for(let i = 0; i < headerAndFooterOpaques.length; i++){
+    headerAndFooterOpaques[i].style.opacity = ".7"
+    setTimeout(()=>{
+      headerAndFooterOpaques[i].style.zIndex = "-1"
+    },200)
+  }
+
+}
+function blackOut(){
+  let headerAndFooterOpaques = document.getElementsByClassName("blackout")
+  for(let i = 0; i < headerAndFooterOpaques.length; i++){
+    headerAndFooterOpaques[i].style.zIndex = "0"
+    headerAndFooterOpaques[i].style.opacity = ".7"
+  }
+}
+function closeMobileNavMenu(){
+  document.querySelector("#menu-expand i").classList.replace("fa-times", "fa-bars")
+  document.getElementById("menu-expand").style.marginRight="16px"
+  unblacken()
+  document.querySelector("main").style.opacity = "1"
+  document.getElementById("nav-header-mobile").style.transform = "translateY(-100%)"
+}
+function openMobileNavMenu(){
+  document.querySelector("#menu-expand i").classList.replace("fa-bars", "fa-times")
+  document.getElementById("menu-expand").style.marginRight = "20px"
+  blackOut()
+  setTimeout(()=>{
+    document.getElementById("nav-header-mobile").style.transform = "translateY(0)"
+  },100)
+}
+function openOrCloseMobileNavMenu(){
+  if(mobileMenuIsOpen){
+    closeMobileNavMenu()
+  }
+  else {
+    openMobileNavMenu()
+  }
+  mobileMenuIsOpen = !mobileMenuIsOpen
 }
 function fontify(query){
   let listItems = document.querySelectorAll(query)
