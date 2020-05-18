62f8e1f953cb43f586e58dbedb37b2be84b83c6f
brandongiampa <30915668+brandongiampa@users.noreply.github.com>
2020-05-18 00:03:03 -0700

    style landing page more

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php include_once 'inc/header.php';?>
     <main>
+      <div class="blackout"></div>
       <div id="hero-image">
         <img src="img/original/buildingcrop.jpg" alt="">
       </div>
-      <h1>¡Bienvenidos a Los Tacos Amigos!</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, pariatur! Assumenda reprehenderit enim saepe nostrum officia placeat quisquam repellat cum quaerat nemo totam dolorem aliquam, minus accusantium sit deserunt beatae magni, dolore atque error aut, omnis! Numquam?</p>
-      <p>Ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quisquam vitae autem omnis nostrum non ab, eius id atque assumenda corrupti eum incidunt quidem aperiam ipsam neque molestiae eveniet sit ea beatae. Iste quos obcaecati veniam, nemo temporibus. Possimus perferendis animi minima?</p>
-      <p>Dolor sit amet, consectetur adipisicing elit. Voluptate autem magnam dolorem suscipit amet facere porro vero illum, ad. Hic omnis illum, fugit nihil, voluptates esse expedita est quaerat laborum officia nemo facilis labore cupiditate.</p>
-      <p>Sit amet, consectetur adipisicing elit. Aliquid qui, nam quidem eaque est, alias. Nihil non iure dolorem sit enim repudiandae.</p>
-      <p>Consectetur adipisicing elit. Id expedita, eveniet. Nostrum inventore cumque laboriosam consequatur, earum commodi eaque impedit nisi beatae quidem perspiciatis ipsa, odio alias ducimus error sit, maiores dignissimos deserunt ad reiciendis!</p>
+      <div class="main-text">
+        <h1>¡Bienvenidos a Los Tacos Amigos!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, pariatur! Assumenda reprehenderit enim saepe nostrum officia placeat quisquam repellat cum quaerat nemo totam dolorem aliquam, minus accusantium sit deserunt beatae magni, dolore atque error aut, omnis! Numquam?</p>
+        <p>Ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quisquam vitae autem omnis nostrum non ab, eius id atque assumenda corrupti eum incidunt quidem aperiam ipsam neque molestiae eveniet sit ea beatae. Iste quos obcaecati veniam, nemo temporibus. Possimus perferendis animi minima?</p>
+        <p>Dolor sit amet, consectetur adipisicing elit. Voluptate autem magnam dolorem suscipit amet facere porro vero illum, ad. Hic omnis illum, fugit nihil, voluptates esse expedita est quaerat laborum officia nemo facilis labore cupiditate.</p>
+        <p>Sit amet, consectetur adipisicing elit. Aliquid qui, nam quidem eaque est, alias. Nihil non iure dolorem sit enim repudiandae.</p>
+        <p>Consectetur adipisicing elit. Id expedita, eveniet. Nostrum inventore cumque laboriosam consequatur, earum commodi eaque impedit nisi beatae quidem perspiciatis ipsa, odio alias ducimus error sit, maiores dignissimos deserunt ad reiciendis!</p>
+      </div>
     </main>
 <?php include_once 'inc/footer.php';?>
