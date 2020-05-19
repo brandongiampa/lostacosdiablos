aa78f194154e4777a4a6600b9cc44864537861bf
brandongiampa <30915668+brandongiampa@users.noreply.github.com>
2020-05-19 02:00:58 -0700

    add temporary content for news page

---
+++ news.php
@@ -0,0 +1,26 @@
+<?php include_once 'inc/header.php';?>
+<main>
+  <div class="news-item">
+    <div class="news-item-date">May 31, 2020</div>
+    <div class="news-item-body">
+      <div class="news-item-img">
+        <img src="img/original/postmates.jpg" alt="">
+      </div>
+      <div class="news-item-text">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias architecto, blanditiis eius, reprehenderit reiciendis voluptas iusto magnam tenetur nostrum quos optio adipisci facere suscipit, cumque!
+      </div>
+    </div>
+  </div>
+  <div class="news-item">
+    <div class="news-item-date">May 31, 2020</div>
+    <div class="news-item-body">
+      <div class="news-item-img">
+        <img src="img/original/rojos.jpg" alt="">
+      </div>
+      <div class="news-item-text">
+        POP UP ALERT! Rojos Hot Chicken at @lostacosamigos 4pm-9pm or Sellout. This Saturday Jan 25th. @rojoshotchicken @lostacosamigos #tennessechicken #friedchicken #ochotchicken #ocfriedchicken #lahotchicken #lafriedchicken #anaheimhotchicken #anaheimfriedchicken #anaheimfoodies #ocfoodies #lafoodies @mexi_papa_adventures @mrbiggsmenu
+      </div>
+    </div>
+  </div>
+</main>
+<?php include_once 'inc/footer.php';?>
