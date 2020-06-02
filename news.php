ae9eb54b63ca62b838c5fe47f7ebcfe0d1156fe3
brandongiampa <30915668+brandongiampa@users.noreply.github.com>
2020-06-02 01:05:57 -0700

    style news page further

--- news.php
+++ news.php
@@ -2,33 +2,37 @@
   $page_hero_image = "news";
   $page_header = "News";
   include_once 'inc/header.php';?>
-  <div class="news-item">
-    <div class="news-item-header">
-      <div class="news-item-date">May 31, 2020</div>
-      <h2>We are on Postmates!</h2>
-    </div>
-    <div class="news-item-body">
-      <div class="news-item-img">
-        <img src="img/original/postmates.jpg" alt="">
+  <a href="news-item.php">
+    <div class="news-item">
+      <div class="news-item-header">
+        <h2>We are on Postmates!</h2>
+        <div class="news-item-date">May 31, 2020</div>
       </div>
-      <div class="news-item-text">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias architecto, blanditiis eius, reprehenderit reiciendis voluptas iusto magnam tenetur nostrum quos optio adipisci facere suscipit, cumque!
-      </div>
-    </div>
-  </div>
-  <div class="news-item">
-    <div class="news-item-header">
-      <div class="news-item-date">May 31, 2020</div>
-      <h2>Los Rojos estan Aqui!</h2>
-    </div>
-    <div class="news-item-body">
-      <div class="news-item-img">
-        <img src="img/original/rojos.jpg" alt="">
-      </div>
-      <div class="news-item-text">
-        POP UP ALERT! Rojos Hot Chicken at @lostacosamigos 4pm-9pm or Sellout. This Saturday Jan 25th. @rojoshotchicken @lostacosamigos #tennessechicken #friedchicken #ochotchicken #ocfriedchicken #lahotchicken #lafriedchicken #anaheimhotchicken #anaheimfriedchicken #anaheimfoodies #ocfoodies #lafoodies @mexi_papa_adventures @mrbiggsmenu
+      <div class="news-item-body">
+        <div class="news-item-img">
+          <img src="img/original/postmates.jpg" alt="">
+        </div>
+        <div class="news-item-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias architecto, blanditiis eius, reprehenderit reiciendis voluptas iusto magnam tenetur nostrum quos optio adipisci facere suscipit, cumque...
+        </div>
       </div>
     </div>
-  </div>
+  </a>
+  <a href="news.item.php">
+    <div class="news-item">
+      <div class="news-item-header">
+        <h2>Los Rojos estan Aqui!</h2>
+        <div class="news-item-date">May 31, 2020</div>
+      </div>
+      <div class="news-item-body">
+        <div class="news-item-img">
+          <img src="img/original/rojos.jpg" alt="">
+        </div>
+        <div class="news-item-text">
+          POP UP ALERT! Rojos Hot Chicken at @lostacosamigos 4pm-9pm or Sellout. This Saturday Jan 25th. @rojoshotchicken @lostacosamigos #tennessechicken #friedchicken #ochotchicken #ocfriedchicken #lahotchicken #lafriedchicken #anaheimhotchicken #anaheimfriedchicken #anaheimfoodies #ocfoodies #lafoodies @mexi_papa_adventures @mrbiggsmenu...
+        </div>
+      </div>
+    </div>
+  </a>
 </main>
 <?php include_once 'inc/footer.php';?>
