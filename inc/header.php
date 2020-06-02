e02754c70c43a67109ce745fa04fee8c39e581f1
brandongiampa <30915668+brandongiampa@users.noreply.github.com>
2020-06-02 03:18:15 -0700

    add language select

--- inc/header.php
+++ inc/header.php
@@ -20,12 +20,19 @@
           <img src="img/logo/newlogocropped.jpg" alt="Los Tacos Diablos">
         </a>
       </div>
+      <div id="lang-expand"><!--
+        <i class="fas fa-globe"></i><span id="language-code">EN<i class="fas fa-caret-down"></i></span>
+-->
+      <i class="fas fa-globe"></i>
+      <select name="language" id="language">
+        <option value="English">EN</option>
+        <option value="Spanish">ES</option>
+        <option value="Korean">KO</option>
+      </select>
+      </div>
       <div id="menu-expand">
         <i class="fas fa-bars"></i>
-      </div><!--
-      <div id="ctas">
-        <a href="#" class="btn btn-red">ORDER NOW</a>
-      </div>-->
+      </div>
       <nav id="nav-header-pc" style="visibility: hidden; position: absolute; height: 0;">
         <ul>
           <li>
